8ddf5fab609986d12af3bcc4a74a2c9476a73207
DiegoFernandoMarinL <160855594+DiegoFernandoMarinL@users.noreply.github.com>
2024-07-08 08:18:15 -0500

    feat: :sparkles: Creacion de clase hija Estudiante
    Se valida que esten conectadas la clase padre con la clase hija

--- js/main.js
+++ js/main.js
@@ -1,30 +1,49 @@
 class Persona {
-    #nombre;
-    #edad;
-    #sexo;
+    nombre;
+    edad;
+    sexo;
     constructor(nombre, edad, sexo) {
-        this.#nombre = nombre;
-        this.#edad = edad;
-        this.#sexo = sexo;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sexo = sexo;
+    }
+    set setNombre(nombre) {
+        this.nombre = nombre;
     }
     get getNombre() {
-        return this.#nombre;
+        return this.nombre;
+    }
+    set setEdad(edad) {
+        this.edad = edad;
     }
     get getEdad() {
-        return this.#edad;
+        return this.edad;
+    }
+    set setSexo(sexo) {
+        this.sexo = sexo;
     }
     get getSexo() {
-        return this.#sexo;
+        return this.sexo;
     }
     saludar() {
-        return `Buen dia ${this.#nombre} espero te encuentres bien`;     
+        return `Buen dia ${this.nombre} espero te encuentres bien`;     
     }
 }
 
-let persona1 = new Persona("Diego","23","Masculino");
+class Estudiante extends Persona{
+    constructor(nombre, edad, sexo, carrera) {
+        super(nombre, edad, sexo);
+        this.carrera = carrera;
+    }
+    get getCarrera() {
+        return this.carrera;
+    }
+    estudiar() {
+        return `${this.saludar()}, tienes ${this.getEdad} años con genero ${this.getSexo}, y estudias ${this.carrera}`
+    }
+}
+let estudiante1 = new Estudiante("Diego","23","Masculino","Programacion");
 
 document.querySelector("#resultado").innerHTML = /*html*/`
-    <h1>Saludo:<span> ${persona1.saludar()}</span></h1>
-    <h1>Edad: <span>${persona1.getEdad}</span></h1>
-    <h1>Sexo: <span>${persona1.getSexo}</span></h1>
+    <h1>Info:<span> ${persona1.estudiar()}</span></h1>
 `;
